c9942c7c5eeb30a304e2c7ef78c99c18f58945f6
GurenLagann <dragonwits@gmail.com>
2020-08-17 10:28:42 -0300

    Formulario atualizado na parte

--- scripts/createa-point.js
+++ scripts/createa-point.js
@@ -3,7 +3,7 @@ function populatesUFs(){
 
   fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
     .then( res => res.json() )
-    .then( states => {
+    .then( states => {  
 
       for (const state of states) {
         ufselect.innerHTML +=  `<option value="${state.id}">${state.nome}</option>`
@@ -24,12 +24,14 @@ function getCities(event) {
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+  citySelect.innerHTML = "<option value>Selecione a Cidade</option"
+  citySelect.disabled = true
+
   fetch(url)
   .then( res => res.json() )
   .then( cities => {
-
     for (const city of cities) {
-      citySelect.innerHTML +=  `<option value="${city.id}">${city.nome}</option>`
+      citySelect.innerHTML +=  `<option value="${city.nome}">${city.nome}</option>`
     }
 
     citySelect.disabled = false
@@ -39,3 +41,29 @@ function getCities(event) {
 document
   .querySelector("select[name=uf]")
   .addEventListener("change", getCities)
+
+// Items de Coleta
+//pegar todos os Li's
+
+const itemsToCollet = document.querySelectorAll(".items-grid li")
+
+for ( const item of itemsToCollet ) {
+  item.addEventListener("click", handleSelectedItem)
+}
+
+let selectedItems = []
+
+function handleSelectedItem(event){
+  //add or remove one class with javaScript
+  const itemLi = event.target
+  const itemId = event.target.dataset.id
+
+  itemLi.classList.toggle("selected")
+
+  //verificando se existem itens selecionados 
+  const alreadySelecetd = selectedItems.findIndex(item => {
+    const intemFound = item ==itemId
+    return intemFound
+  })
+  
+}
